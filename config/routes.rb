8d70a609946fa6ffd82825ca7fdad3b854300cb8
Anthony Lee <antwonlee@gmail.com>
2017-02-23 21:08:20 -0600

    Add OmniAuth Salesforce
    Because:
    
    + We need to authenticate the app with Salesforce credientials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "homepages#show"
+
   devise_for :users
+  # devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  # controllers: {omniauth_callbacks: "omniauth_callbacks"}
 end
