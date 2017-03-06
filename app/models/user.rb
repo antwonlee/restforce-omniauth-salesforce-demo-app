b8dd87cc9b49a7cbe348f964375ac1538a5eaeb7
Anthony Lee <antwonlee@gmail.com>
2017-03-05 21:33:42 -0600

    Add before_filter to initiate restforce
    + Testing out all sorts of options

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,19 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.instance_url = auth.credentials.instance_url
       user.oauth_token = auth.credentials.token
-      user.refresh_token = auth.credentials.refresh_token
+      user.refresh_token = URI.unescape(auth.credentials.refresh_token)
       user.save!
+    end
+  end
+
+  def restforce
+    if oauth_token && refresh_token && instance_url
+      @restforce ||= Restforce.new :oauth_token => oauth_token,
+        :refresh_token           => refresh_token,
+        :instance_url            => instance_url,
+        :client_id               => ENV["SALESFORCE_CLIENT_ID"],
+        :client_secret           => ENV["SALESFORCE_CLIENT_SECRET"],
+        :authentication_callback => Proc.new {|x| Rails.logger.debug x.to_s}
     end
   end
 end
